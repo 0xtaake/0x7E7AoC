86c0025b231d2dcecf6923c73de855fbce4ebf1b
taake <taake@tfwno.gf>
2023-12-01 22:03:06 +0000

    Debug

--- day01/1.py
+++ day01/1.py
@@ -4,17 +4,18 @@ def open_file():
 
 j = 0
 
+dict = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+dict2 = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
 with open_file() as file:
     k = file.readlines()
     i = 0
     while i < 1000:
-        str = ""
+        str = []
         f = k[i]
-        for m in f:
-            if m.isdigit():
-                str += m
-        str2 = str[0] + str[-1]
-        j += int(str2)
-        i += 1
-
+        if f in dict2:
+            str.append(dict[f])
+            str2 = str[0] + str[-1]
+            j += str2
+        i += 1        
 print(j)
