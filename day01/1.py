c0354cf1efc31bf132d587b2378bc5a5f154fceb
taake <taake@tfwno.gf>
2023-12-01 21:45:51 +0000

    Debug

--- day01/1.py
+++ day01/1.py
@@ -2,20 +2,19 @@ def open_file():
     f = open("input","r")
     return f
 
-def check_sum_line(line):
+j = 0
+
+with open_file() as file:
+    k = file.readlines()
     i = 0
-    for j in line:
-        if j.isnumeric():
-            i += int(j)
-    return i
+    while i < 1000:
+        str = ""
+        f = k[i]
+        for m in f:
+            if m.isdigit():
+                str += m
+        str2 = str[0] + str[-1]
+        j += int(str2)
+        i += 1
 
-i = 0
-
-f = open_file()
-
-lines = f.readlines()
-
-for line in lines:
-    i += int(check_sum_line(line))
-
-print(i)
+print(j)
